e1518c9079ab13460b16ad83e2605293ce695679
danji404 <mire090909@gmail.com>
2025-04-12 02:38:41 +0900

    설정 컴포넌트

--- TAPE CORP/Assets/Scripts/Order/OrderJudge.cs
+++ TAPE CORP/Assets/Scripts/Order/OrderJudge.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OrderJudge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("주문 검증 대상")]
+    public Transform checkButtonTransform; // 버튼 위치
+    public float checkRadius = 0.5f;
+    public LayerMask itemLayer;
+
+    public void CheckOrders()
     {
-        
+        Collider2D hit = Physics2D.OverlapCircle(checkButtonTransform.position, checkRadius, itemLayer);
+        if (hit == null) return;
+
+        OrderItem item = hit.GetComponent<OrderItem>();
+        if (item == null) return;
+
+        OrderBubbleUI[] orders = FindObjectsOfType<OrderBubbleUI>();
+        foreach (var order in orders)
+        {
+            if (IsMatch(item, order.data))
+            {
+                MoneyManager money = FindObjectOfType<MoneyManager>();
+                money.AddMoney(100); // 정답 보상
+                Destroy(order.gameObject); // 주문 완료
+                Destroy(item.gameObject); // 아이템 제거
+                break;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsMatch(OrderItem item, OrderData data)
     {
-        
+        return item.itemName == data.itemName &&
+               Mathf.Approximately(item.size.x, data.itemSize.x) &&
+               Mathf.Approximately(item.size.y, data.itemSize.y);
     }
 }
--- TAPE CORP/Assets/Scripts/Order/OrderSpawner.cs
+++ TAPE CORP/Assets/Scripts/Order/OrderSpawner.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class OrderSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("주문 UI 프리팹 및 부모")]
+    public GameObject orderBubblePrefab;
+    public Transform bubbleParent;
 
-    // Update is called once per frame
-    void Update()
+    [Header("생성 가능한 주문 리스트")]
+    public List<OrderData> possibleOrders;
+
+    public void SpawnRandomOrder()
     {
-        
+        if (possibleOrders.Count == 0) return;
+
+        int index = Random.Range(0, possibleOrders.Count);
+        OrderData selected = possibleOrders[index];
+
+        GameObject go = Instantiate(orderBubblePrefab, bubbleParent);
+        OrderBubbleUI bubble = go.GetComponent<OrderBubbleUI>();
+
+        // UI 정렬을 위해 기존 UI 밀기
+        foreach (Transform child in bubbleParent)
+        {
+            if (child != go.transform)
+                child.localPosition += new Vector3(200, 0, 0); // 오른쪽으로 밀기
+        }
+
+        // 난이도에 따라 시간 설정 (1~2분)
+        float sizeFactor = (selected.boxSize.x + selected.boxSize.y + selected.itemSize.x + selected.itemSize.y) / 4f;
+        selected.timeLimit = Mathf.Clamp(60f + sizeFactor * 10f, 60f, 120f);
+
+        bubble.Init(selected);
     }
 }
