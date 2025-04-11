c32820ba231da0c3dc703221d0abd7e06deacadf
jaewonjung6446 <jaewon6446@naver.com>
2025-04-12 02:51:56 +0900

    레일 구현

--- TAPE CORP/Assets/Scripts/BackGround/Conveyor/Conveyor.cs
+++ TAPE CORP/Assets/Scripts/BackGround/Conveyor/Conveyor.cs
@@ -4,15 +4,36 @@ public class Conveyor : MonoBehaviour
 {
     public float speed = 1.5f; // 컨베이어 벨트의 속도
     public bool isRight = true;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        // Rigidbody2D 컴포넌트를 가진 물체에 대해서만 속도를 적용
         Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
-        if (rb != null)
+
+        if (rb != null && rb.gameObject.CompareTag("Holder"))
         {
-            // 컨베이어 벨트의 방향으로 속도 적용
+            // 중력 제거
+            rb.gravityScale = 0;
+
+            // 로컬 기준 오른쪽 방향으로 velocity 설정
+            Vector2 localRight = transform.right.normalized;
+            rb.velocity = localRight * speed;
+        }
+        else if (rb != null)
+        {
+            // 이 부분은 절대 건드리지 않음
             Vector2 velocity = (isRight ? Vector2.right * Time.deltaTime : Vector2.left * Time.deltaTime);
             collision.transform.Translate(velocity * speed);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+
+        if (rb != null && rb.gameObject.CompareTag("Holder"))
+        {
+            // 중력 다시 적용
+            rb.gravityScale = 1;
         }
     }
 }
